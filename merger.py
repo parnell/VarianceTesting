6c6e48c6f475e00af03ba0ca10c2741d60261d7f
parnell <parnell@Lisas-and-Now-Lees-MacBook-Pro.local>
2017-01-20 13:34:13 -0600

    Merger complete

--- merger.py
+++ merger.py
@@ -1,11 +1,31 @@
 import sys
+import re
+import collections
 
-
+def key(line):
+    s = line.split('\t')
+    print('{}_{}_{}_{}_{}'.format(
+        s[0], # dim
+        s[1], # size
+        s[2], # K
+        s[3], # var
+        s[4] # nclus
+        ))
 
 if __name__ == "__main__":
     files = []
     ks = {}
+    lines = collections.OrderedDict()
     for f in sys.argv:
-        ks[f.split('_')[-1]] = f
+        n = f.split('_')[-1]
+        n = re.sub(r'\D','',n)
+        ks[n] = f
     for k in sorted(ks):
-        print(k)
+        # print(k,ks[k])
+        for line in open(ks[k]):
+            m = re.search('statline',line)
+            if m:
+                lines[key(line)] = line
+
+    for line in lines.values():
+        print(line)
