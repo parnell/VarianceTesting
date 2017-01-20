820c4efe5c2d3635bc44eac094457315ef9529cd
parnell <parnell@Lisas-and-Now-Lees-MacBook-Pro.local>
2017-01-20 13:41:29 -0600

    key fixes

--- merger.py
+++ merger.py
@@ -5,15 +5,17 @@ import collections
 def key(line):
     s = line.split('\t')
     try:
-        print('{}_{}_{}_{}_{}'.format(
+        k = '{}_{}_{}_{}_{}'.format(
             s[0], # dim
             s[1], # size
             s[2], # K
             s[3], # var
             s[4] # nclus
-            ))
+            )
+        return k
     except:
         print('failed', line, file=sys.stderr)
+        return ''
 
 if __name__ == "__main__":
     files = []
